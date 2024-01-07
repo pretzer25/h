8eeb887507c55d8b63629b7641cfeca66c80cbad
pretzer25 <153140996+pretzer25@users.noreply.github.com>
2024-01-07 12:21:18 -0600

    Delete static directory

--- static/worksheets/api/searchengine.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('searchengine').value = localStorage.getItem('searchengine');
-});
-document.getElementById('searchEngineForm').addEventListener('submit', function(event) {
-  searchEngineSetting = document.getElementById('searchengine').value;
-  localStorage.setItem('searchengine', searchEngineSetting);
-  alert(searchEngineSetting + ' is now your default search engine!')
-});
